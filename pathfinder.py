2573f460235ffa2f1f77f89ef35e75dfdebd0559
Nicholas Bor <nicholasbor@Nicholass-MacBook-Pro.local>
2021-09-01 13:41:40 +1000

    create file and make window for pathfinder

---
+++ pathfinder.py
@@ -0,0 +1,23 @@
+import pygame
+
+WIDTH = 800
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Pathfinder")
+
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+running = True
+  
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
