d28569868b53aa30cf9fe0f3fc9a37f59acdcff5
Nicholas Bor <nicholasbor@Nicholass-MacBook-Pro.local>
2021-12-31 11:29:59 +1100

    can now set start, end and barriers in grid

--- pathfinder.py
+++ pathfinder.py
@@ -5,7 +5,7 @@ WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Pathfinder")
 
 BLACK = (0, 0, 0)
-GRAY = (127, 127, 127)
+GREY = (127, 127, 127)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -58,14 +58,77 @@ def make_grid(rows, width):
         grid.append([])
         for col in range(rows):
             cell = Cell(row, col, gap, rows)
-            grid[col].append(cell)
+            grid[row].append(cell)
 
     return grid
 
+def draw_grid(window, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(window, GREY, (0, i * gap), (width, i * gap))
+        pygame.draw.line(window, GREY, (i * gap, 0), (i * gap, width))
+
+def draw(window, grid, rows, width):
+    window.fill(WHITE)
+
+    for row in grid:
+        for i in row:
+            i.draw(window)
+    
+    draw_grid(window, rows, width)
+    pygame.display.update()
+
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+
+
+rows = 80
+grid = make_grid(rows, WIDTH)
+
+start = None
+end = None
+
+started = False
 running = True
   
 while running:
+    draw(WINDOW, grid, rows, WIDTH)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+        if started:
+            continue
+
+        if pygame.mouse.get_pressed()[0]:
+            pos = pygame.mouse.get_pos()
+            row, col = get_clicked_pos(pos, rows, WIDTH)
+            cell = grid[row][col]
+            if not start and cell != end:
+                start = cell
+                start.set_start()
+            
+            elif not end and cell != start:
+                end = cell
+                end.set_end()
+
+            elif cell != start and cell != end:
+                cell.set_barrier()
+
+        elif pygame.mouse.get_pressed()[2]:
+            pos = pygame.mouse.get_pos()
+            row, col = get_clicked_pos(pos, rows, WIDTH)
+            cell = grid[row][col]
+            cell.clear()
+            if cell == start:
+                start = None
+            elif cell == end:
+                end = None
 
